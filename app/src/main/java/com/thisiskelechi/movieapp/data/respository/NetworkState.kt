646ca9536ee24cef48586729f7878bc0420ff047
riskymind <oparaiskelechi@gmail.com>
2020-03-08 16:30:47 +0100

    create the network class for handling network conditions

--- app/src/main/java/com/thisiskelechi/movieapp/data/respository/NetworkState.kt
+++ app/src/main/java/com/thisiskelechi/movieapp/data/respository/NetworkState.kt
@@ -9,6 +9,10 @@ enum class Status {
 class NetworkState(val status: Status, val msg: String) {
 
     companion object {
+        val LOADED: NetworkState = NetworkState(Status.SUCCESS, "Success")
+        val LOADING: NetworkState = NetworkState(Status.RUNNING, "Running")
+        val ERROR: NetworkState = NetworkState(Status.FAILED, "Something went wrong")
 
     }
+
 }
